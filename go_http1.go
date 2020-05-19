101cb923b163ad85bad7ed32e2f4f2e9e345e328
haeunsong <62704334+haeunsong@users.noreply.github.com>
2020-05-19 16:16:00 +0900

    middleware logging
    net/http 패키지의 HandleFunc 메소드를 통해 요청에 대한 서버측 Logging 구현 예제
    cf. https://niceman.tistory.com/153?category=995401

---
+++ go_http1.go
@@ -0,0 +1,54 @@
+
+// cf. https://niceman.tistory.com/153?category=995401
+// go - net/http 미들웨어 - Log 사용 예제
+// 주소창에 ex> localhost:9090/req-test1 입력
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// 미들웨어 함수 선언(Return : func)
+func logging(f http.HandlerFunc)http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		log.Println("Middleware Test(Log): ",r.URL.Path)
+		f(w,r)
+	}
+}
+
+// 요청1 핸들러
+func req_test1(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "Requested : (/req-test1)")
+}
+
+// 요청2 핸들러
+func req_test2(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "Requested : (/req-test2)")
+}
+
+func main(){
+	// 핸들러 메소드 지정1
+	http.HandleFunc("/req-test1",logging(req_test1))
+	// 핸들러 메소드 지정2
+	http.HandleFunc("/req-test2",logging(req_test2))
+
+	// 기본 출력
+	fmt.Println("Golang WebServer Working!")
+
+	// 서버 시작
+	http.ListenAndServe(":9090",nil)
+}
+
+
+
+
+
+
+
+
+
+
+
+
